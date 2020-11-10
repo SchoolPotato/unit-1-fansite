20646747123dc2dbd36043585fa14ca7ac804789
Alex Welton <awelto389@west-mec.org>
2020-11-09 18:22:15 -0700

    Fixed Dark mode Button, fixed Text

--- JS/index.js
+++ JS/index.js
@@ -10,6 +10,9 @@ function darkModeSwitch(){
     var navbarElement = document.getElementsByClassName("navbar");
     var darkNavbarElement = document.getElementsByClassName("navbar-darkmode");
     var topbarElement = document.getElementsByTagName("img");
+    var menuElement = document.getElementsByClassName("menu");
+    var darkMenuElement = document.getElementsByClassName("menu-darkmode");
+    var buttonElement = document.getElementsByTagName("button");
     
     //changing elements to darkmode elements
     if (bodyElement[0].className == "main"){
@@ -22,7 +25,11 @@ function darkModeSwitch(){
         navbarElement[0].className = "navbar-darkmode";
         linkElements[0].id = "navbar-darkmode";
         linkElements[1].id = "navbar-darkmode";
+        linkElements[2].id = "menu-darkmode";
+        linkElements[3].id = "menu-darkmode";
         topbarElement[0].src = "Images/header-background-darkmode.png";
+        menuElement[0].className = "menu-darkmode";
+        buttonElement[0].id = "button-darkmode";
     }
 
     //changing elements back to lightmode
@@ -36,6 +43,22 @@ function darkModeSwitch(){
         darkNavbarElement[0].className = "navbar";
         linkElements[0].id = "navbar";
         linkElements[1].id = "navbar";
+        linkElements[2].id = "menu";
+        linkElements[3].id = "menu";
         topbarElement[0].src = "Images/header-background.png";
+        darkMenuElement[0].className = "menu";
+        buttonElement[0].id = "button";
     }
 }
+
+function toggleMenu() {
+    var x = document.getElementsByClassName("menu");
+    var y = document.getElementsByClassName("dark-menu");
+    if (x.style.display == "none" || y.style.display == "none") {
+      x[0].style.display = "block";
+      y[0].style.display = "block";
+    } else {
+      x[0].style.display = "none";
+      y[0].style.display = "none";
+    }
+  }
